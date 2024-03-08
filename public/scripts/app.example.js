b7775692a33fe67e736cc40d320e1304feda859f
JevinLeon <jevinleon@gmail.com>
2024-03-08 21:30:24 +0700

    Challenge 2.2

---
+++ public/scripts/app.example.js
@@ -0,0 +1,91 @@
+class App {
+  constructor() {
+    this.clearButton = document.getElementById("clear-btn");
+    this.loadButton = document.getElementById("load-btn");
+    this.carContainerElement = document.getElementById("cars-container");
+
+    this.searchBtn = document.querySelector(".search-btn");
+    this.carContainer = document.querySelector(".car-container");
+  }
+
+  async init() {
+    await this.load();
+    // Register click listener
+    // this.clearButton.onclick = this.clear;
+    // this.loadButton.onclick = this.run;
+
+    this.searchBtn.onclick = () => {
+      let selectedDate = document.getElementById("tanggal").value;
+      let selectedTime = document.getElementById("waktu").value;
+      let passengerCount = document.getElementById("jumlah").value;
+
+      this.filterCar(selectedDate, selectedTime, passengerCount);
+    };
+  }
+
+  run = () => {
+    Car.list.forEach((car) => {
+      if (car.available) {
+        const node = document.createElement("div");
+        node.classList.add("car-card");
+        node.innerHTML = car.render();
+        this.carContainer.appendChild(node);
+      }
+    });
+  };
+
+  async load() {
+    const cars = await Binar.listCars();
+    Car.init(cars);
+  }
+
+  clear = () => {
+    let child = this.carContainer.firstElementChild;
+
+    while (child) {
+      child.remove();
+      child = this.carContainer.firstElementChild;
+    }
+  };
+
+  async filterCar(date, time, passengerCount) {
+    if (!date && !time && !passengerCount) {
+      this.clear();
+      this.load();
+      this.run();
+    } else {
+      let filteredCar;
+      if (isNaN(passengerCount)) {
+        filteredCar = await Binar.listCars(
+          (car) =>
+            this.formatDate(car.availableAt) >= date &&
+            car.availableAt.getHours() >= time
+        );
+      } else {
+        filteredCar = await Binar.listCars(
+          (car) =>
+            this.formatDate(car.availableAt) >= date &&
+            car.availableAt.getHours() >= time &&
+            car.capacity >= passengerCount
+        );
+      }
+      console.log(filteredCar);
+
+      Car.init(filteredCar);
+      this.clear();
+      this.run();
+    }
+  }
+
+  formatDate(date) {
+    var d = new Date(date),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [year, month, day].join("-");
+  }
+}
---
+++ public/scripts/car.example.js
@@ -0,0 +1,80 @@
+class Car {
+  static list = [];
+
+  static init(cars) {
+    this.list = cars.map((i) => new this(i));
+  }
+
+  constructor({
+    id,
+    plate,
+    manufacture,
+    model,
+    image,
+    rentPerDay,
+    capacity,
+    description,
+    transmission,
+    available,
+    type,
+    year,
+    options,
+    specs,
+    availableAt,
+  }) {
+    this.id = id;
+    this.plate = plate;
+    this.manufacture = manufacture;
+    this.model = model;
+    this.image = image;
+    this.rentPerDay = rentPerDay;
+    this.capacity = capacity;
+    this.description = description;
+    this.transmission = transmission;
+    this.available = available;
+    this.type = type;
+    this.year = year;
+    this.options = options;
+    this.specs = specs;
+    this.availableAt = availableAt;
+  }
+
+  render() {
+    return `
+
+    <img
+      src="${this.image}"
+      class="img-fluid car-item-img object-fit-cover mb-3"
+      alt="car"
+    />
+    <div class="d-flex flex-column gap-1">
+      <h6 class="fw-medium">${this.manufacture}</h6>
+      <h6 class="fw-bold fs-5">Rp ${this.rentPerDay} / hari</h6>
+      <p class="car-item-desc">
+        ${this.description}
+      </p>
+      <div class="row">
+        <div class="col-1">
+          <img src="./images/people-logo.png" alt="people-logo" />
+        </div>
+        <div class="col">${this.capacity} orang</div>
+      </div>
+      <div class="row">
+        <div class="col-1">
+          <img src="./images/settings-logo.png" alt="settings-logo" />
+        </div>
+        <div class="col">${this.transmission}</div>
+      </div>
+      <div class="row">
+        <div class="col-1">
+          <img src="./images/calendar-logo.png" alt="calendar-logo" />
+        </div>
+        <div class="col">Tahun ${this.year}</div>
+      </div>
+      <div class="mt-3">
+        <button class="custom-btn w-100">Pilih Mobil</button>
+      </div>
+    </div>
+    `;
+  }
+}
